2824e850c767f1f2af6b45339d66c519fc49ced0
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-08-25 16:28:56 -0500

    switched out DOM for reactstrap COl Row components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
+      <Container>
+        <Row>
+          <Col md="12">
             This is the Header
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col md="8">
             This is the Video Player
             <div className="videoPlayer">
               VIdeo Plays here
@@ -21,27 +22,27 @@ function App() {
             <div className="search">
               Search bar
             </div>
-          </div>
+          </Col>
           <div className="col-md-4 col-md-offset-4"> 
             This is the Twitter Feeds
           </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
             This is the rocket Launch list
               <div>Launch Data</div>
               <div>Launch Data</div>
               <div>Launch Data</div>
               <div>Launch Data</div>
               <div>Launch Data</div>
-          </div>
-          <div className="col-md-8">
+          </Col>
+          <Col md="8">
             This is the ISS Coordinates
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
