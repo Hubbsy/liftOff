b98dfd8991fb05f3bd1b414ae895929a658ebe03
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-08-25 21:32:29 -0500

    added basic component structure and created component hierarchy

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Col} from 'reactstrap';
+
+
+const Header = () => {
+  return (
+    <Col md="12">
+        This is the Header
+    </Col>
+  )
+}
+
+
+
+
+export default Header;
---
+++ src/TwitterFeeds.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Col} from 'reactstrap'; 
+
+
+const TwitterFeeds = () => {
+  return (
+    <Col m={{ size: 'auto', offset: 4 }}> 
+      This is the Twitter Feeds
+    </Col>
+  )
+}
+
+
+
+
+export default TwitterFeeds; 
+
+// className="col-md-4 col-md-offset-4"
