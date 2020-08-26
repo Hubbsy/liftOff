5c46ff04f6102f11b30facaab72d860fbcfdd2f3
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-08-25 23:07:40 -0500

    removed rows and added into header

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ function App() {
   return (
     <div className="App">
       <Container>
-        <Row>
           <Header />
-        </Row>
       </Container>
       <Container>
         <Row>
