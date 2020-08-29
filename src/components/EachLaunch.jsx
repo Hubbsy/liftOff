8888cf6122c076551e0f9f308976b3be4d44dec0
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-08-29 15:15:20 -0500

    successfully rendering live data for each of 5 upcoming rocket launches

--- src/components/EachLaunch.jsx
+++ src/components/EachLaunch.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 import { Col, Card, CardHeader, CardBody, CardTitle, CardText, Button, CardFooter, UncontrolledCollapse, CardLink } from 'reactstrap';
 
 const EachLaunch = (props) => {
-  console.log(props.launches);
+  console.log(props.launches.quicktext);
+
+ 
+
+
+  // let liveLink = function returnLink() {
+  //   let string = props.launches.quicktext;
+  //   let link = string.substr(60, string.length - 1); 
+  //   let result;
+  //   for (var i = 0; i <= link.length; i++) {
+  //     if (link.includes('https')) {
+  //        result = link.slice(link.indexOf('https'), -1);
+  //     } 
+  //   }
+  //   return result; 
+  // }
+
+
+
   return (
     <Col>
       <div>
@@ -14,12 +32,12 @@ const EachLaunch = (props) => {
               <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
               <UncontrolledCollapse toggler="#toggler">
                 <CardText>
-                  A SpaceX Falcon 9 rocket will launch the SAOCOM-1B mission on Friday, August 28, 2020 at 11:19 PM (UTC). This Launch will liftoff from Cape Canaveral Florida in the United States.
+                   {props.launches.launch_description} This Launch will Lift Off from {props.launches.pad.location.name} in {props.launches.pad.location.country}.
                 </CardText>
               </UncontrolledCollapse>
             </CardBody>
           <CardFooter className="text-muted">
-            <CardLink href="https://rocketlaunch.live/launch/saocom-1b">Live Feed</CardLink>
+            <CardLink href="https://www.rocketlaunch.live/">Live Feed</CardLink>
           </CardFooter>
         </Card>
       </div>
