d2a0640a55642a4541f85b71b06ecf7bd466ce47
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-08-29 14:38:09 -0500

    transformed Launches to class component and mapping through API response

--- src/components/EachLaunch.jsx
+++ src/components/EachLaunch.jsx
@@ -1,78 +1,15 @@
 import React from 'react';
 import { Col, Card, CardHeader, CardBody, CardTitle, CardText, Button, CardFooter, UncontrolledCollapse, CardLink } from 'reactstrap';
 
-const EachLaunch = () => {
+const EachLaunch = (props) => {
+  console.log(props.launches);
   return (
     <Col>
       <div>
         <Card>
           <CardHeader tag="h3">Countdown</CardHeader>
             <CardBody>
-              <CardTitle>SpaceX</CardTitle>
-              {/* <CardText>Today's launch is scheduled for ... weather is expected to be ...</CardText> */}
-              <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <CardText>
-                  A SpaceX Falcon 9 rocket will launch the SAOCOM-1B mission on Friday, August 28, 2020 at 11:19 PM (UTC). This Launch will liftoff from Cape Canaveral Florida in the United States.
-                </CardText>
-              </UncontrolledCollapse>
-            </CardBody>
-          <CardFooter className="text-muted">
-            <CardLink href="https://rocketlaunch.live/launch/saocom-1b">Live Feed</CardLink>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader tag="h3">Countdown</CardHeader>
-            <CardBody>
-              <CardTitle>SpaceX</CardTitle>
-              {/* <CardText>Today's launch is scheduled for ... weather is expected to be ...</CardText> */}
-              <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <CardText>
-                  A SpaceX Falcon 9 rocket will launch the SAOCOM-1B mission on Friday, August 28, 2020 at 11:19 PM (UTC). This Launch will liftoff from Cape Canaveral Florida in the United States.
-                </CardText>
-              </UncontrolledCollapse>
-            </CardBody>
-          <CardFooter className="text-muted">
-            <CardLink href="https://rocketlaunch.live/launch/saocom-1b">Live Feed</CardLink>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader tag="h3">Countdown</CardHeader>
-            <CardBody>
-              <CardTitle>SpaceX</CardTitle>
-              {/* <CardText>Today's launch is scheduled for ... weather is expected to be ...</CardText> */}
-              <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <CardText>
-                  A SpaceX Falcon 9 rocket will launch the SAOCOM-1B mission on Friday, August 28, 2020 at 11:19 PM (UTC). This Launch will liftoff from Cape Canaveral Florida in the United States.
-                </CardText>
-              </UncontrolledCollapse>
-            </CardBody>
-          <CardFooter className="text-muted">
-            <CardLink href="https://rocketlaunch.live/launch/saocom-1b">Live Feed</CardLink>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader tag="h3">Countdown</CardHeader>
-            <CardBody>
-              <CardTitle>SpaceX</CardTitle>
-              {/* <CardText>Today's launch is scheduled for ... weather is expected to be ...</CardText> */}
-              <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
-              <UncontrolledCollapse toggler="#toggler">
-                <CardText>
-                  A SpaceX Falcon 9 rocket will launch the SAOCOM-1B mission on Friday, August 28, 2020 at 11:19 PM (UTC). This Launch will liftoff from Cape Canaveral Florida in the United States.
-                </CardText>
-              </UncontrolledCollapse>
-            </CardBody>
-          <CardFooter className="text-muted">
-            <CardLink href="https://rocketlaunch.live/launch/saocom-1b">Live Feed</CardLink>
-          </CardFooter>
-        </Card>
-        <Card>
-          <CardHeader tag="h3">Countdown</CardHeader>
-            <CardBody>
-              <CardTitle>SpaceX</CardTitle>
+              <CardTitle>{props.launches.provider.name}</CardTitle>
               {/* <CardText>Today's launch is scheduled for ... weather is expected to be ...</CardText> */}
               <Button color="info" id="toggler" style={{ marginBottom: '1rem' }}>More Info</Button>
               <UncontrolledCollapse toggler="#toggler">
--- src/components/Launches.jsx
+++ src/components/Launches.jsx
@@ -1,14 +1,53 @@
 import React from 'react';
-import {Col} from 'reactstrap';
+import {Col, ListGroup, ListGroupItem} from 'reactstrap';
 import EachLaunch from './EachLaunch';
+import axios from 'axios';
 
 
-const Launches = () => {
-  return (
-    <Col>
-      <EachLaunch />
-    </Col>
-  )
+class Launches extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      launches: []
+    }
+    this.getLaunches = this.getLaunches.bind(this);
+  }
+
+
+  componentDidMount() {
+    this.getLaunches();
+  }
+
+   async getLaunches() {
+    await axios.get('https://fdo.rocketlaunch.live/json/launches/next/5')
+    .then(response => {
+      return response.data.result;
+    })
+    .then(data => {
+      this.setState ({
+        launches: data
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+
+  render() {
+    return (
+      <Col>
+      <ListGroup>
+      {this.state.launches.map((launch) => {
+        return (
+          <EachLaunch launches={launch} />
+        )
+      })}  
+      </ListGroup>
+      </Col>
+    )
+  }
+
 };
 
 
