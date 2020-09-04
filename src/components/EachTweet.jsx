cfb82e504e598cff2101448933cf267e9cca0e74
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-09-04 17:48:53 -0500

    added fresh card layour and fixed image for tweets from socket.io

--- src/components/EachTweet.jsx
+++ src/components/EachTweet.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Card, CardBody, CardText, CardLink, CardTitle, CardSubtitle } from 'reactstrap';
+import { Col, Card, CardBody, CardText, CardLink, CardSubtitle, CardImg } from 'reactstrap';
 
 
 const EachTweet = (props) => {
@@ -7,37 +7,16 @@ const EachTweet = (props) => {
   return (
     
       <Col>
-      <Card>
-        <CardBody>
-          <CardTitle>{props.tweets.user}</CardTitle>
-          <CardSubtitle>{props.tweets.date}</CardSubtitle>
-          <CardText>{props.tweets.text}</CardText>
-          <CardLink href="#">{props.tweets.user}</CardLink>
-        </CardBody>
-      </Card>
-    
-      {/* <div> */}
-                {/* <div className="card-panel grey lighten-5 z-depth-3 hoverable thin">
-                    <div className="row valign-wrapper">
-                        <div className="col s2">
-                            <img   className="circle responsive-img" />
-                        </div>
-                        <div className="col s10 left-align">
-                            <span className="black-text"></span>
-                        </div>
-                        
-                    </div>
-                    <div className="row valign-wrapper right-align chip hoverable">
-                    {new Date().toLocaleTimeString()}
-                    </div>
-                    <div className="row valign-wrapper right-align chip hoverable">
-                        <a href={`https://twitter.com/`} target="_blank"></a>
-                    </div>
-                </div>
-
-            </div> */}
-      
-        </Col>
+        <Card className="tweet">
+          
+          <CardBody>
+            <CardImg id="pic"  src={props.tweets.image} alt="Card image cap"/>
+            <CardLink href={`http://twitter.com/${props.tweets.url}`} target="_blank">{props.tweets.user}</CardLink>
+            <CardText>{props.tweets.text}</CardText>
+            <CardSubtitle> Posted at: {props.tweets.date}</CardSubtitle>
+          </CardBody>
+        </Card>
+      </Col>
   )
 }
 
