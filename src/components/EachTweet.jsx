117b10c08c40f19dcde7d97ebf03f6d374cd0116
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-09-04 19:11:07 -0500

    adjusted size of each tweet and viewfinder but switched back to orig

--- src/components/EachTweet.jsx
+++ src/components/EachTweet.jsx
@@ -11,7 +11,7 @@ const EachTweet = (props) => {
           
           <CardBody>
             <CardImg id="pic"  src={props.tweets.image} alt="Card image cap"/>
-            <CardLink href={`http://twitter.com/${props.tweets.url}`} target="_blank">{props.tweets.user}</CardLink>
+            <CardLink href={`http://twitter.com/${props.tweets.url}`} target="_blank">@{props.tweets.user}</CardLink>
             <CardText>{props.tweets.text}</CardText>
             <CardSubtitle> Posted at: {props.tweets.date}</CardSubtitle>
           </CardBody>
--- src/components/Station.jsx
+++ src/components/Station.jsx
@@ -57,7 +57,7 @@ class Station extends React.Component {
        html: '<span></span>'
      });
     let marker = this.marker = L.marker([0,0], {icon:issIcon}).addTo(this.map);
-    marker.bindPopup("This is the most recent position of the ISS in Low Earth Orbit (LEO)").openPopup();
+    marker.bindPopup("This is the most recent position of the ISS in Low Earth Orbit").openPopup();
     setInterval(()=> this.locateISS(marker),10000);
   };
 
