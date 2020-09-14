4db708c01633d3b2f1c6d52186892c1afc48b394
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-09-13 21:24:38 -0500

    added conditional rendering in case APOD is not updated

--- src/components/DailyImage.jsx
+++ src/components/DailyImage.jsx
@@ -30,14 +30,20 @@ class DailyImage extends React.Component {
 
 
   render() {
-    
-    return (
-      <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
-        <img className="jumbotron" src={this.state.image.hdurl} alt="the NASA APOD is currently being updated" /> 
-      </a>  
-    )
+    if (this.state.image.hdurl) {
+      return (
+        <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
+          <img className="jumbotron" src={this.state.image.hdurl} alt="the NASA APOD is currently being updated" /> 
+        </a>  
+      )
+    } else {
+      return (
+        <a href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
+          <img className="jumbotron" src="https://www.teslarati.com/wp-content/uploads/2018/02/Falcon-Heavy-side-booster-landings-SpaceX-e1518547599594.jpg" alt="the NASA APOD is currently being updated" /> 
+        </a>  
+      )
+    }
   }
-
 }
 
 
