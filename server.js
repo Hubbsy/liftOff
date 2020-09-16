96e081202c85be4aa233818d662dd7a6a44192e3
Justin Hubbell <hubbsy@justins-mbp.lan>
2020-09-16 17:49:40 -0500

    adjusted file structure and added heroku scripts for deployment

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const Twit = require('twit');
+const io = require('socket.io').listen(server);
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(cors());
+app.use(helmet.frameguard({action: 'SAMEORIGIN'}));
+
+
+let follow = ['11348282', '14091091'];//NASA twitter id's
+
+
+
+let T = new Twit({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+
+  io.sockets.on('connection', function(socket) {
+
+    let stream = T.stream('statuses/filter', { follow: follow, language: 'en' });
+
+    stream.on('tweet', function(tweet) {
+        io.sockets.emit('stream', {
+          image: tweet.user.profile_image_url_https,
+          date: new Date(tweet.created_at).toLocaleTimeString(),
+          text: tweet.text, 
+          user: tweet.user.screen_name,
+          url: tweet.user.screen_name
+        })
+    });
+  });
+
+
+  if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Houston we have A Connection at port ${PORT}`);
+});
