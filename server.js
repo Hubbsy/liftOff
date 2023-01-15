570ff3e5ef1442970fae092bb3b3ee5f3d576eca
Justin Hubbell <55633110+Hubbsy@users.noreply.github.com>
2023-01-14 23:39:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(helmet.frameguard({action: 'SAMEORIGIN'}));
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
